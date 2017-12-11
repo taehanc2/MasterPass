0725b5e24696acab4241e85ad90c904436924649
Proxycannon <tonylu1595@gmail.com>
2017-12-11 04:12:18 -0600

    Basic encryption

---
+++ MasterPassword/src/password/Encryptor.java
@@ -0,0 +1,105 @@
+package password;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Encryptor {
+	public static String encrypt(String key, String initVector, String value) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+			byte[] encrypted = cipher.doFinal(value.getBytes());
+			
+
+			return Base64.encodeBase64String(encrypted);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static String decrypt(String key, String initVector, String encrypted) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+			byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+			return new String(original);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+		String key = MD5("hello"); // 128 bit key
+		String initVector = "0000000000000000"; // 16 bytes IV
+
+		System.out.println(decrypt(key, initVector, encrypt(key,initVector,"Website: what\nUserID: is\nPassword: this")));
+		
+	}
+
+	public static String MD5(String text) {
+		String passwordToHash = text;
+		String generatedPassword = null;
+		try {
+			// Create MessageDigest instance for MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			//Add password bytes to digest
+			md.update(passwordToHash.getBytes());
+			//Get the hash's bytes
+			byte[] bytes = md.digest();
+			//This bytes[] has bytes in decimal format;
+			//Convert it to hexadecimal format
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i< bytes.length ;i++)
+			{
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			//Get complete hashed password in hex format
+			generatedPassword = sb.toString().substring(16);
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+	
+	public static String sha256(String base) {
+	    try{
+	        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	        byte[] hash = digest.digest(base.getBytes("UTF-8"));
+	        StringBuffer hexString = new StringBuffer();
+
+	        for (int i = 0; i < hash.length; i++) {
+	            String hex = Integer.toHexString(0xff & hash[i]);
+	            if(hex.length() == 1) hexString.append('0');
+	            hexString.append(hex);
+	        }
+
+	        return hexString.toString();
+	    } catch(Exception ex){
+	       throw new RuntimeException(ex);
+	    }
+	}
+
+}
+
+
--- MasterPassword/src/password/MasterPassword.java
+++ MasterPassword/src/password/MasterPassword.java
@@ -1,5 +1,6 @@
 package password;
 
+import java.util.Random;
 import java.util.Scanner;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
@@ -10,27 +11,23 @@ public class MasterPassword {
 	private boolean hasSetup;
 	public Scanner scan;
 	public Scanner scan2;
-	public Scanner scan3;
 	private String ultpassword;
 	private String trypassword;
 	private String copyOfText;
 	private int count;
+	//	private Random random;
 	File in;
 	File out;
 	String input;
 
 	public MasterPassword() throws IOException {
+		//		random = new Random();
 		scan = new Scanner(System.in);
 		out = new File("/home/proxycannon/git/MasterPass/MasterPassword/new.txt");		
 		copyOfText = FileUtils.readFileToString(out, "UTF-8");		
 		more = true;
 		scan2 = new Scanner(copyOfText);
-		hasSetup = scan2.hasNext();
-		
-		//out.delete();
-		
-		
-		
+		hasSetup = scan2.hasNext();				
 	}	
 
 	public void run() throws IOException {
@@ -40,86 +37,120 @@ public class MasterPassword {
 			String input = scan.next();
 			System.out.println("Please enter the password again");
 			String input2 = scan.next();
+			String hashed = Encryptor.sha256(input);
 			if (input.equals(input2)) {
-				FileUtils.writeStringToFile(out, (input + "\n"), "UTF-8", true);
+				FileUtils.writeStringToFile(out, (hashed + "\n"), "UTF-8", true);
 				hasSetup = true;
 			} else {
 				System.out.println("Passwords do not match! Please try again.");
-			}
-			
+			}			
 		}
+
+		// Checks password
 		copyOfText = FileUtils.readFileToString(out, "UTF-8");	
 		scan2 = new Scanner(copyOfText);
-		ultpassword = scan2.next();
+		ultpassword = scan2.nextLine();
 		System.out.println("Enter your password: ");
-		this.trypassword = scan.next();
+		this.trypassword = Encryptor.sha256(scan.next());
 		while (!trypassword.equals(ultpassword)) {
 			System.out.println("Incorrect Password! Try again: ");
-			this.trypassword = scan.next();
+			this.trypassword = Encryptor.sha256(scan.next());
 		}
+
+		String key = Encryptor.MD5(ultpassword);
+		String temp = "";
+		scan2 = new Scanner(copyOfText);
+		count = 0;
+		while (scan2.hasNextLine()) {
+			String wow = scan2.nextLine();
+			if (count > 0) {
+				temp += wow;				
+			}
+			count++;
+		}
+		copyOfText = temp;
+
+		copyOfText = Encryptor.decrypt(key, "0000000000000000", copyOfText);
+		//		byte[] randombytes = new byte[8];
+		//		random.nextBytes(randombytes);		
+		//		String initVector = new String(randombytes);
+
 		while (more) {
-			more = false;
-			
-			
+			more = false;			
+
 			//TODO: Implement encryption password with hash
 			System.out.println("1. View your accounts \n2. Store new accounts \n3. Wipe data"
 					+ " \n---------------------");
 			int option = scan.nextInt();
 			if (option == 1) {
 				count = 0;
-				copyOfText = FileUtils.readFileToString(out, "UTF-8");
 				scan2 = new Scanner(copyOfText);
 				//Recall all the accounts that are stored
-				while (scan2.hasNext()) {					
-					String value = scan2.nextLine();
-					if (count > 0) {
-						System.out.println(value);
-					}
+				while (scan2.hasNextLine()) {					
+					String value = scan2.nextLine();					
+					System.out.println(value);
 					count++;
+				}
+				System.out.println("Do you want to do more: ");
+				String answer = scan.next();
+				if (answer.toLowerCase().equals("y") 
+						|| answer.toLowerCase().equals("ye")
+						|| answer.toLowerCase().equals("yes")) {
+					more = true;
 				}
 			} else if (option == 2) {				
 				System.out.println("Type your name of the website: ");
 				input = "Website: " + scan.next() + "\n";
-				FileUtils.writeStringToFile(out, input, "UTF-8", true);
-				
+				copyOfText += input;
+
 				System.out.println("Type your ID: ");
 				input = "UserID: " + scan.next() + "\n";
-				FileUtils.writeStringToFile(out, input, "UTF-8", true);
-				
+				copyOfText += input;
+
 				System.out.println("Type your password: ");
 				input = "Password: " + scan.next() + "\n\n";
-				FileUtils.writeStringToFile(out, input, "UTF-8", true);
-				
+				copyOfText += input;
+
 				System.out.println("Do you want to do more: ");
-			    String answer = scan.next();
-			    if (answer.toLowerCase().equals("y") 
-			    		|| answer.toLowerCase().equals("ye")
-			    		|| answer.toLowerCase().equals("yes")) {
-			    	more = true;
-			    }
+				String answer = scan.next();
+				if (answer.toLowerCase().equals("y") 
+						|| answer.toLowerCase().equals("ye")
+						|| answer.toLowerCase().equals("yes")) {
+					more = true;
+				}
 			} else if (option == 3) {
 				out.delete();
 				FileUtils.writeStringToFile(out, "", "UTF-8", true);
-				
+				System.exit(0);
+
 			} else {
 				System.out.println("Invalid number, try again");
 				more = true;
 			}
-			//				} else {
-			//					System.out.println("Please run again and get the password right");
-			//				}
-			
+
 		}
-	}
-	
+		String finalCopyOfText = "";
+		scan2 = new Scanner(copyOfText);	
+		count = 0;
+		while (scan2.hasNextLine()) {
+			String wow = scan2.nextLine();
+			finalCopyOfText += wow + "\n";	
+			count++;
+		}
 		
+		finalCopyOfText = Encryptor.encrypt(key, "0000000000000000", finalCopyOfText);
+
+		FileUtils.writeStringToFile(out, (ultpassword + "\n"), "UTF-8", false);
+		FileUtils.writeStringToFile(out, finalCopyOfText, "UTF-8", true);
+	}
+
 
 	public static void main(String[] args) throws IOException {
-		
+
 		MasterPassword mypass = new MasterPassword();
 		mypass.run();
-		
+
 	}
-	
+
 }
 
