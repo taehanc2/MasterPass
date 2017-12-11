629948aad58480c2aaab3446a55d95bd484bf44a
Proxycannon <tonylu1595@gmail.com>
2017-12-10 20:42:08 -0600

    Need to implement encryption and multiple user accounts

--- MasterPassword/src/password/MasterPassword.java
+++ MasterPassword/src/password/MasterPassword.java
@@ -1,68 +1,125 @@
 package password;
+
 import java.util.Scanner;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
 
 public class MasterPassword {
+	private boolean more;
+	private boolean hasSetup;
+	public Scanner scan;
+	public Scanner scan2;
+	public Scanner scan3;
+	private String ultpassword;
+	private String trypassword;
+	private String copyOfText;
+	private int count;
+	File in;
+	File out;
+	String input;
 
-	private String ID;
-	private String password;
-	private String website;
-	
-	public MasterPassword() {
+	public MasterPassword() throws IOException {
+		scan = new Scanner(System.in);
+		out = new File("/home/proxycannon/git/MasterPass/MasterPassword/new.txt");		
+		copyOfText = FileUtils.readFileToString(out, "UTF-8");		
+		more = true;
+		scan2 = new Scanner(copyOfText);
+		hasSetup = scan2.hasNext();
 		
-	}
-	
-	public MasterPassword(String ID, String Password, String website) {
-		this.ID = ID;
-		this.password = Password;
-		this.website = website;
-	}
-	
-	public static String ultpassword;
-	
-	public static String nameofweb;
-	
-	public static String nameofid;
-	
-	public static String nameofpass;
-	
-	public static void main(String[] args) {
-		MasterPassword mypass = new MasterPassword();
+		//out.delete();
 		
-		String password1;
-		String data;
-		int option = 0;
-		mypass.password = "";
-		data = "";
-		Scanner scan = new Scanner(System.in);
-		while (ultpassword == null || ultpassword.isEmpty()) {
-			System.out.println("Please setup the password!");			
-			ultpassword = scan.nextLine();
-		} 
-		if (ultpassword != null && !ultpassword.isEmpty()) {
-			System.out.println("Type your password to view");
-			
-			password1 = scan.nextLine();
-			if (password1.equals(ultpassword)) {
-				System.out.println("type 1 to view your accounts, or 2 to store new accounts");
-				option = scan.nextInt();
-				if (option == 1) {
-					//Recall all the accounts that are stored
-					System.out.println("sh");
-				} else if (option == 2) {
-					//store new account
-					System.out.println("Type your name of the website");
-					nameofid = scan.nextLine();
-					System.out.println("Type your ID");
-					nameofpass = scan.nextLine();
-					nameofweb = scan.nextLine();
-					// put these data into constructors
-				} else {
-					//wrong number so error
-					System.out.println("You entered wrong number, try again from beginning");
-				}
+		
+		
+	}	
+
+	public void run() throws IOException {
+		// Checks to see if password has been setup
+		while (!hasSetup) {
+			System.out.println("Please set up a password: ");
+			String input = scan.next();
+			System.out.println("Please enter the password again");
+			String input2 = scan.next();
+			if (input.equals(input2)) {
+				FileUtils.writeStringToFile(out, (input + "\n"), "UTF-8", true);
+				hasSetup = true;
 			} else {
-				System.out.println("Please run again and get the password right");
+				System.out.println("Passwords do not match! Please try again.");
 			}
+			
+		}
+		copyOfText = FileUtils.readFileToString(out, "UTF-8");	
+		scan2 = new Scanner(copyOfText);
+		ultpassword = scan2.next();
+		System.out.println("Enter your password: ");
+		this.trypassword = scan.next();
+		while (!trypassword.equals(ultpassword)) {
+			System.out.println("Incorrect Password! Try again: ");
+			this.trypassword = scan.next();
+		}
+		while (more) {
+			more = false;
+			
+			
+			//TODO: Implement encryption password with hash
+			System.out.println("1. View your accounts \n2. Store new accounts \n3. Wipe data"
+					+ " \n---------------------");
+			int option = scan.nextInt();
+			if (option == 1) {
+				count = 0;
+				copyOfText = FileUtils.readFileToString(out, "UTF-8");
+				scan2 = new Scanner(copyOfText);
+				//Recall all the accounts that are stored
+				while (scan2.hasNext()) {					
+					String value = scan2.nextLine();
+					if (count > 0) {
+						System.out.println(value);
+					}
+					count++;
+				}
+			} else if (option == 2) {				
+				System.out.println("Type your name of the website: ");
+				input = "Website: " + scan.next() + "\n";
+				FileUtils.writeStringToFile(out, input, "UTF-8", true);
+				
+				System.out.println("Type your ID: ");
+				input = "UserID: " + scan.next() + "\n";
+				FileUtils.writeStringToFile(out, input, "UTF-8", true);
+				
+				System.out.println("Type your password: ");
+				input = "Password: " + scan.next() + "\n\n";
+				FileUtils.writeStringToFile(out, input, "UTF-8", true);
+				
+				System.out.println("Do you want to do more: ");
+			    String answer = scan.next();
+			    if (answer.toLowerCase().equals("y") 
+			    		|| answer.toLowerCase().equals("ye")
+			    		|| answer.toLowerCase().equals("yes")) {
+			    	more = true;
+			    }
+			} else if (option == 3) {
+				out.delete();
+				FileUtils.writeStringToFile(out, "", "UTF-8", true);
+				
+			} else {
+				System.out.println("Invalid number, try again");
+				more = true;
+			}
+			//				} else {
+			//					System.out.println("Please run again and get the password right");
+			//				}
+			
 		}
 	}
+	
+		
+
+	public static void main(String[] args) throws IOException {
+		
+		MasterPassword mypass = new MasterPassword();
+		mypass.run();
+		
+	}
+	
 }
+
